cbc0da796c098dc571ed1467db4f1e94bdc66936
Sheikh Faraz <gamingwithfaraz569.com>
2025-03-14 18:05:37 +0500

    Removed not found page or file

--- src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFoundPage() {
-    return(
-        <div>
-            <h1>Page not found please return to previous page Or refresh the page</h1>
-        </div>
-    )
-}
